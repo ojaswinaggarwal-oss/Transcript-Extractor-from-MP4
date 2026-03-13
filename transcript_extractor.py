3b232be8708455f8c3f4dd14052993bca729f50a
ojaswinaggarwal-oss <ojaswinaggarwal@gmail.com>
2026-03-13 12:32:19 +0530

    Correction

--- transcript_extractor.py
+++ transcript_extractor.py
@@ -1,9 +1,9 @@
 from faster_whisper import WhisperModel
 
-Video_Path = "UI Path Beginner to Intermediate - ManishKala.mp4"
+Video_Path = "-----------"
 
 model = WhisperModel("tiny")
-segments, info = model.transcribe("Video_Path")
+segments, info = model.transcribe(Video_Path)
 
 with open("transcript.txt", "w", encoding="utf-8") as f:
     for segment in segments:
